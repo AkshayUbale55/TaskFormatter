22c78dc1564677a605f7bbdf496b914a5575cd96
Ramesh Alishetty <saipranayalishetty@gmail.com>
2023-05-22 22:19:02 +0530

    Button UI changes

--- src/Content/Button14.js
+++ src/Content/Button14.js
@@ -148,7 +148,7 @@ const Buttons14 = (props) => {
                             onMouseLeave={() => {
                                 setDropDown(false);
                             }}
-                            className="fixed bg-white shadow-lg border-2 top-[19rem]  py-2 rounded-md px-4 w-auto z-30"
+                            className="fixed bg-white shadow-lg border-2 top-[17rem]  py-2 rounded-md px-4 w-auto z-30"
                         >
                             <ul className="flex-col justify-center items-center">
                                 <li className="flex py-2 hover:text-[#00857C] text-black text-md items-center font-medium border-b-2" onClick={copyVeevaBuild}>
@@ -163,7 +163,7 @@ const Buttons14 = (props) => {
                                 
                             </ul>
                         </div>}
-                        <button className='flex justify-evenly m-auto text-center text-white bg-[#1b9a92] border-0 p-2 focus:outline-none hover:bg-[#00857C] rounded text-xs'
+                        <button className='flex justify-evenly m-auto text-center text-white bg-[#1b9a92] border-0 p-2 focus:outline-none hover:bg-[#00857C] rounded text-xs w-[12.2rem]'
                         >
                             {props.name}
                         </button>
